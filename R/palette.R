16cc4ab10e76149b51171627ed12b0e2580c8c2c
stephen-l-jones <stephen.l.jones@gmail.com>
2022-07-06 22:45:16 -0700

    wip

--- R/palette.R
+++ R/palette.R
@@ -1,26 +1,146 @@
+#' Colorblind-safe color palettes
+#'
+#' \code{colorsafe_palette} returns the named color palette.
+#'
+#' @param palette
+#' Name of palette. Run \code{palettes()} to see palette names and hex codes.
+#' @param n
+#' Number of colors to return. \code{n} may not be greater than the maximum
+#' number colors for a given palette and chroma.
+#' @param chroma
+#' Color chroma based on HCL colorspace. Any of \code{"high"}, \code{"low"},
+#' or \code{"variable"}.
+#' @param grayscale_safe
+#' If \code{TRUE}, palette must be grayscale safe.
+#' @param reverse
+#' Reverse the order of the color palette.
+#' @param luminance_range
+#' Minimum and maximum luminance values in HCL colorspace.
+#' @param max_chroma
+#' Maximum chroma in HCL colorspace. Applicable when
+#' \code{chroma = "variable"}.
+#' @return
+#' A color palette function, palette, or list of palettes (see description).
+#' @examples
+#' pal <- colorsafe_palette("Paired 6")
+#' colorspace::swatchplot(pal)
 #' @export
 colorsafe_palette <- function(
-  name, n, chroma = c("high","low","variable"), grayscale_safe = FALSE, reverse = FALSE, ...
+    palette, n, chroma = c("high","low","variable"), grayscale_safe = FALSE,
+    reverse = FALSE, luminance_range = c(20, 90), max_chroma = 105
 ) {
   chroma <- match.arg(chroma, several.ok = TRUE)
   palette_list(
-    name, n = n, chroma = chroma, grayscale_safe = grayscale_safe, reverse = reverse, ...
+    palette, n = n, chroma = chroma, grayscale_safe = grayscale_safe, reverse = reverse,
+    luminance_range = luminance_range, max_chroma = max_chroma
   )[[1]]
 }
 
+#' @rdname colorsafe_palette
+#' @description
+#'\code{discrete_palette} returns a palette function for discrete scales.
 #' @export
-colorsafe <- function(
-  name, chroma = c("high","low","variable"), grayscale_safe = FALSE, reverse = FALSE, ...
+discrete_palette <- function(
+    palette, chroma = c("high","low","variable"), grayscale_safe = FALSE,
+    reverse = FALSE, luminance_range = c(20, 90), max_chroma = 105
 ) {
   function (n) {
-    colorsafe_palette(name, n, chroma, grayscale_safe, reverse, ...)
+    colorsafe_palette(palette, n, chroma, grayscale_safe, reverse, luminance_range, max_chroma)
   }
 }
 
+#' @rdname colorsafe_palette
+#' @description
+#'\code{diverging_palette} returns a two-color diverging palette function.
+#' @param hue_pair
+#' Name of a diverging palette.
+#' @examples
+#'
+#' pal <- diverging_palette()(5)
+#' colorspace::swatchplot(pal)
+#' pal <- diverging_palette(reverse = TRUE, luminance_range = c(50,97))(5)
+#' colorspace::swatchplot(pal)
+#' pal <- diverging_palette("Chartreuse-Magenta", luminance_range = c(40,90))(6)
+#' colorspace::swatchplot(pal, cvd = c("deutan","protan"))
+#' @export
+diverging_palette <- function(
+    hue_pair = "Red-Blue", chroma = "variable",
+    reverse = FALSE, luminance_range = c(20, 90), max_chroma = 105
+) {
+  pals <- unique(palette_master[palette_master$type == "diverging", "palette_name"])
+  if (!hue_pair %in% pals)
+    stop("Diverging palette not found. Must be one of: %s", paste(pals, collapse = ", "))
+  function (n) {
+    colorsafe_palette(hue_pair, n, "variable", FALSE, reverse, luminance_range, max_chroma)
+  }
+}
+
+#' @rdname colorsafe_palette
+#' @description
+#'\code{qualitative_palette} returns a multi-hue qualitative palette function.
+#' @param name
+#' Name of a qualitative palette.
+#' @examples
+#'
+#' pal <- qualitative_palette()(5)
+#' colorspace::swatchplot(pal)
+#' pal <- qualitative_palette("Set 2.5")(5)
+#' colorspace::swatchplot(pal)
+#' pal <- qualitative_palette(chroma = "low", grayscale_safe = TRUE)(5)
+#' colorspace::swatchplot(pal, cvd = c("deutan","protan","desaturate"))
+#' @export
+qualitative_palette <- function(
+  name = "Set 1.5", chroma = c("high","low","variable"), grayscale_safe = FALSE,
+  reverse = FALSE
+) {
+  pals <- unique(palette_master[palette_master$type == "qualitative", "palette_name"])
+  if (!name %in% pals)
+    stop("Qualitative palette not found. Must be one of: %s", paste(pals, collapse = ", "))
+  function (n) {
+    colorsafe_palette(name, n, chroma, grayscale_safe, reverse)
+  }
+}
+
+#' @rdname colorsafe_palette
+#' @description
+#'\code{sequential_palette} returns a single-hue sequential palette function.
+#' @param hue
+#' Name of a seqential palette.
+#' @examples
+#'
+#' pal <- sequential_palette()(4)
+#' colorspace::swatchplot(pal)
+#' pal <- sequential_palette(chroma = "variable", luminance_range = c(50,97))(4)
+#' colorspace::swatchplot(pal)
+#' pal <- sequential_palette(chroma = "low")(4)
+#' colorspace::swatchplot(pal)
+#' @export
+sequential_palette <- function(
+    hue = "Red", chroma = c("high","low","variable"), grayscale_safe = FALSE,
+    reverse = FALSE, luminance_range = c(20, 90), max_chroma = 105
+) {
+  pals <- unique(palette_master[palette_master$type == "sequential", "palette_name"])
+  if (!hue %in% pals)
+    stop("Sequential palette not found. Must be one of: %s", paste(pals, collapse = ", "))
+  function (n) {
+    colorsafe_palette(hue, n, chroma, grayscale_safe, reverse, luminance_range, max_chroma)
+  }
+}
+
+#' @rdname colorsafe_palette
+#' @description
+#'\code{palettes} returns a list of available palettes.
+#' @param type
+#' Type of palette. Must be \code{"qualitative"},  \code{"sequential"}, or
+#' \code{"diverging"}.
+#' @examples
+#'
+#' names(palettes(type = "diverging", chroma = "variable"))
+#' palettes(5, type = "qualitative", grayscale_safe = TRUE)
 #' @export
 palettes <- function(
-  n, type = c("sequential","diverging","qualitative"), chroma = c("high","low","variable"),
-  grayscale_safe = FALSE, reverse = FALSE, ...
+  n, type = c("qualitative","sequential","diverging"), chroma = c("high","low","variable"),
+  grayscale_safe = FALSE, reverse = FALSE, luminance_range = c(20, 90), max_chroma = 105
 ) {
   type <- match.arg(type, several.ok = TRUE)
   chroma <- match.arg(chroma, several.ok = TRUE)
@@ -28,7 +148,8 @@ palettes <- function(
   pals <- lapply(
     type,
     palette_list,
-    name = NULL, n = n, chroma = chroma, grayscale_safe = grayscale_safe, reverse = reverse, ...
+    name = NULL, n = n, chroma = chroma, grayscale_safe = grayscale_safe, reverse = reverse,
+    luminance_range = luminance_range, max_chroma = max_chroma
   )
   names(pals) <- type
   if (length(pals) == 1) {
@@ -38,7 +159,8 @@ palettes <- function(
 }
 
 palette_list <- function (
-    name, type, n, chroma = c("high","low","variable"), grayscale_safe = FALSE, reverse = FALSE, ...
+    name, type, n, chroma = c("high","low","variable"), grayscale_safe = FALSE, reverse = FALSE,
+    luminance_range = c(20, 90), max_chroma = 105
 ) {
   chroma <- match.arg(chroma, several.ok = TRUE)
 
@@ -58,7 +180,7 @@ palette_list <- function (
   }
   pals <- pals[pals$chroma %in% chroma,]
   if (nrow(pals) == 0)
-    stop("No palette found.")
+    stop(sprintf("No palette found (n=%s).", n))
 
   hex <- list()
   for (i in seq_len(nrow(pals))) {
@@ -67,13 +189,13 @@ palette_list <- function (
     } else {
       palette_n <- n
     }
-    HCL <- get_hcl(pals[i, "palette_code"], palette_n, reverse, ...)
+    HCL <- get_hcl(pals[i, "palette_code"], palette_n, reverse, luminance_range, max_chroma)
     if (length(chroma) > 1) {
       name <- paste0(pals[i, "palette_name"], " (", pals[i, "chroma"], ")")
     } else {
       name <- pals[i, "palette_name"]
     }
-    hex[[name]] <- hcl(HCL[,"H"], HCL[,"C"], HCL[,"L"], fixup = FALSE)
+    hex[[name]] <- hcl(HCL[,"H"], HCL[,"C"], HCL[,"L"], fixup = TRUE)
   }
   return(hex)
 }
@@ -93,7 +215,7 @@ get_hcl <- function (
     ]
     pal <- pal[pal$n == min(pal$n),]
     if (nrow(pal) == 0)
-      stop("Palette not found.")
+      stop(sprintf("Palette not found (n=%s).", n))
 
     palette_hcl <- cbind(
       H = pal[,"H"],
--- R/scales.R
+++ R/scales.R
@@ -1,120 +1,199 @@
-scale_fill_colorsafe_continuous <- function (
-    hue = "Red", chroma = "variable", grayscale_safe = FALSE, reverse = FALSE, ...
-) {
-    ggplot2::continuous_scale(
-      "fill", "colorsafe_continuous",
-      scales::gradient_n_pal(colorsafe_palette(
-        name   = hue,
-        chroma = chroma,
-        grayscale_safe = grayscale_safe,
-        reverse = reverse,
-        ...)),
-      ...
-    )
-}
-
-scale_colour_colorsafe_continuous <- function (
-  ..., hue = "Red", chroma = "variable", grayscale_safe = FALSE, reverse = FALSE
+#' Colorblind-safe color scales
+#'
+#' The \code{colorsafe} scales are colorblind safe and most palettes safely
+#' convert to grayscale.
+#'
+#' @param ...
+#' Other arguments passed \code{ggplot2} functions \code{discrete_scale()},
+#' \code{continuous_scale()}, or \code{binned_scale()}.
+#' @param aesthetic
+#' \code{"colour"} or \code{"fill"} aesthetic.
+#' @param palette
+#' Name of palette. Run \code{\link{palettes}()} to see palette names and hex
+#' codes.
+#' @param chroma
+#' Color chroma based on HCL colorspace. Any of \code{"high"}, \code{"low"},
+#' or \code{"variable"}.
+#' @param grayscale_safe
+#' If \code{TRUE}, palette must be grayscale safe.
+#' @param reverse
+#' Reverse the order of the color palette.
+#' @param luminance_range
+#' Minimum and maximum luminance values in HCL colorspace.
+#' @param max_chroma
+#' Maximum chroma in HCL colorspace. Applicable when
+#' \code{chroma = "variable"}.
+#' @examples
+#' p <- ggplot(faithfuld, aes(waiting, eruptions, fill = density)) +
+#'        geom_tile()
+#' p + scale_fill_colorsafe_binned()
+#' p + scale_fill_colorsafe_continuous(reverse = TRUE)
+#'
+#' p <- ggplot(mtcars, aes(mpg, wt)) +
+#'        geom_point(aes(colour = factor(cyl)), size = 2)
+#' p + scale_colorsafe_discrete(palette = "Set 1.4", chroma = "high")
+#' @export
+scale_colorsafe_continuous <- function (
+  ..., aesthetic = "colour", palette = "Red", chroma = "variable", grayscale_safe = FALSE,
+  reverse = FALSE, luminance_range = c(20, 90), max_chroma = 105
 ) {
   ggplot2::continuous_scale(
-    "colour", "colorsafe_continuous",
+    aesthetic, "colorsafe_continuous",
     scales::gradient_n_pal(colorsafe_palette(
-      name   = hue,
+      palette = palette,
       chroma = chroma,
       grayscale_safe = grayscale_safe,
       reverse = reverse,
-      ...)),
+      luminance_range = luminance_range,
+      max_chroma = max_chroma)),
     ...
   )
 }
 
+#' @rdname scale_colorsafe_continuous
+#' @export
 scale_color_colorsafe_continuous <- function (
-  ..., hue = "Red", chroma = "variable", grayscale_safe = FALSE, reverse = FALSE
+  ..., palette = "Red", chroma = "variable", grayscale_safe = FALSE, reverse = FALSE,
+  luminance_range = c(20, 90), max_chroma = 105
 ) {
-  scale_colour_colorsafe_continuous(
+  scale_colorsafe_continuous(
     ...,
-    hue     = hue,
-    chroma  = chroma,
+    aesthetic  = "colour",
+    palette    = palette,
+    chroma     = chroma,
     grayscale_safe = grayscale_safe,
-    reverse = reverse
+    reverse    = reverse,
+    luminance_range = luminance_range,
+    max_chroma = max_chroma
   )
 }
 
-scale_fill_colorsafe_discrete <- function (
-  ..., palette = "Set 1.5", chroma = "high", grayscale_safe = FALSE, reverse = FALSE
+#' @rdname scale_colorsafe_continuous
+#' @export
+scale_fill_colorsafe_continuous <- function (
+  ..., palette = "Red", chroma = "variable", grayscale_safe = FALSE, reverse = FALSE,
+  luminance_range = c(20, 90), max_chroma = 105
 ) {
-  ggplot2::discrete_scale("fill", "colorsafe_discrete", colorsafe(
-    palette = palette,
-    chroma  = chroma,
+  scale_colorsafe_continuous(
+    ...,
+    aesthetic  = "fill",
+    palette    = palette,
+    chroma     = chroma,
     grayscale_safe = grayscale_safe,
-    reverse = reverse,
-    ...
-  ), ...)
+    reverse    = reverse,
+    luminance_range = luminance_range,
+    max_chroma = max_chroma
+  )
 }
 
+#' @rdname scale_colorsafe_continuous
+#' @export
+scale_colorsafe_discrete <- function (
+  ..., aesthetic = "colour", palette = "Set 1.6", chroma = "low",
+  grayscale_safe = FALSE, reverse = FALSE, luminance_range = c(20, 90), max_chroma = 105
+) {
+  ggplot2::discrete_scale(
+    aesthetic, "colorsafe_discrete",
+    discrete_palette(
+      palette    = palette,
+      chroma     = chroma,
+      grayscale_safe = grayscale_safe,
+      reverse    = reverse,
+      luminance_range = luminance_range,
+      max_chroma = max_chroma
+    ),
+    ...
+  )
+}
+
+#' @rdname scale_colorsafe_continuous
+#' @export
 scale_colour_colorsafe_discrete <- function (
-  ..., palette = "Set 1.5", chroma = "high", grayscale_safe = FALSE, reverse = FALSE
+  ..., palette = "Set 1.6", chroma = "low", grayscale_safe = FALSE, reverse = FALSE,
+  luminance_range = c(20, 90), max_chroma = 105
 ) {
-  ggplot2::discrete_scale("colour", "colorsafe_discrete", colorsafe(
-    palette = palette,
-    chroma  = chroma,
-    grayscale_safe = grayscale_safe,
-    reverse = reverse,
-    ...
-  ), ...)
-}
-
-scale_color_colorsafe_discrete <- function (
-  ..., name = "Set 1.5", chroma = "high", grayscale_safe = FALSE, reverse = FALSE
-) {
-  scale_colour_colorsafe_discrete(
+  scale_colorsafe_discrete(
     ...,
-    palette = palette,
-    chroma  = chroma,
+    aesthetic  = "colour",
+    palette    = palette,
+    chroma     = chroma,
     grayscale_safe = grayscale_safe,
-    reverse = reverse,
-
+    reverse    = reverse,
+    luminance_range = luminance_range,
+    max_chroma = max_chroma
   )
 }
 
-scale_fill_colorsafe_binned <- function (
-  ..., hue = "Red", chroma = "variable", grayscale_safe = FALSE, reverse = FALSE
+#' @rdname scale_colorsafe_continuous
+#' @export
+scale_fill_colorsafe_discrete <- function (
+    ..., palette = "Set 1.6", chroma = "low", grayscale_safe = FALSE, reverse = FALSE,
+    luminance_range = c(20, 90), max_chroma = 105
+) {
+  scale_colorsafe_discrete(
+    ...,
+    aesthetic  = "fill",
+    palette    = palette,
+    chroma     = chroma,
+    grayscale_safe = grayscale_safe,
+    reverse    = reverse,
+    luminance_range = luminance_range,
+    max_chroma = max_chroma
+  )
+}
+
+#' @rdname scale_colorsafe_continuous
+#' @export
+scale_colorsafe_binned <- function (
+  ..., aesthetic = "colour", palette = "Red-Blue", chroma = "variable", grayscale_safe = FALSE,
+  reverse = FALSE, luminance_range = c(20, 90), max_chroma = 105
 ) {
   ggplot2::binned_scale(
-    "fill", "colorsafe_binned",
+    aesthetic, "colorsafe_binned",
     scales::gradient_n_pal(colorsafe_palette(
-      palette = hue,
+      palette = palette,
       chroma  = chroma,
       grayscale_safe = grayscale_safe,
       reverse = reverse,
-      ...)),
+      luminance_range = luminance_range,
+      max_chroma = max_chroma
+      )),
     ...
   )
 }
 
-scale_colour_colorsafe_binned <- function (
-  ..., hue = "Red", chroma = "variable", grayscale_safe = FALSE, reverse = FALSE
-) {
-  ggplot2::binned_scale(
-    "colour", "colorsafe_binned",
-    scales::gradient_n_pal(colorsafe_palette(
-      palette = hue,
-      chroma  = chroma,
-      grayscale_safe = grayscale_safe,
-      reverse = reverse,
-      ...)),
-    ...
-  )
-}
-
+#' @rdname scale_colorsafe_continuous
+#' @export
 scale_color_colorsafe_binned <- function (
-  ..., hue = "Red", chroma = "variable", grayscale_safe = FALSE, reverse = FALSE
+  ..., palette = "Red-Blue", chroma = "variable", grayscale_safe = FALSE, reverse = FALSE,
+  luminance_range = c(20, 90), max_chroma = 105
 ) {
-  scale_colour_colorsafe_binned(
-    hue     = hue,
-    chroma  = chroma,
+  scale_colorsafe_binned(
+    ...,
+    aesthetic  = "colour",
+    palette    = palette,
+    chroma     = chroma,
     grayscale_safe = grayscale_safe,
-    reverse = reverse,
-    ...
+    reverse    = reverse,
+    luminance_range = luminance_range,
+    max_chroma = max_chroma
+  )
+}
+
+#' @rdname scale_colorsafe_continuous
+#' @export
+scale_fill_colorsafe_binned <- function (
+    ..., palette = "Red-Blue", chroma = "variable", grayscale_safe = FALSE, reverse = FALSE,
+    luminance_range = c(20, 90), max_chroma = 105
+) {
+  scale_colorsafe_binned(
+    ...,
+    aesthetic  = "fill",
+    palette    = palette,
+    chroma     = chroma,
+    grayscale_safe = grayscale_safe,
+    reverse    = reverse,
+    luminance_range = luminance_range,
+    max_chroma = max_chroma
   )
 }
